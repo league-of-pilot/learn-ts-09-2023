1c9794dc44347d199e479537c5b760a60d330425
anom <dgg.photo.drive@gmail.com>
2023-09-29 16:53:36 +0700

    feat: setup node-ts watch dev

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-const { bsTsMethod } = require("./bsTs/init_bsts")
+// const { bsTsMethod } = require("./bsTs/init_bsts");
 // const { decoHouse } = require("./decoHouse")
 // const ExampleClass = require("./demo2")
 
-const http = require("http")
+import { tzOrigin } from "./src/timezone/tzOrigin";
+import http from "http";
 
-const hostname = "127.0.0.1"
-const port = 3000
+const hostname = "127.0.0.1";
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain")
-  res.end("Hello World")
-})
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-  bsTsMethod()
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log("🚀 index L21-tzOrigin", tzOrigin.length);
+});
